115ff3bc40a0c75bdfd4f326e586dceeb080fb79
Akshay Singh <asing012@plattsburgh.edu>
2017-09-19 20:21:45 -0400

    Added more prints

--- gs.py
+++ gs.py
@@ -1,8 +1,15 @@
 #!/usr/bin/env python3
-#
-#
-#
-#
+# GS-Shapley Algorithm
+# Created by: Akshay Singh and 
+# Date: 
+# Purpose: 
+# INPUT(S)::
+# OUTPUT(S):
+# EXAMPLES:
+
+
+import time
+import sys
 from random import randrange
 
 guyIndex = {0: "John", 1:"Jack", 2:"Jim", 3:"Kapi", 4:"Rhys", 5:"Oman", 6:"Kirk", 7:"Erik", 8:"Yaz", 9:"Amos"}
@@ -22,6 +29,7 @@ def knuth_shuffle(x):
 	return x
 #--------------------------------------
 
+
 def free_Men(fm):
 	for man in guyPrefer.keys():
 		fm.append(man)
@@ -30,9 +38,9 @@ def free_Men(fm):
 def matching(fm,engaged,girlEngaged):
 	while(len(fm) > 0):
 		for man in guyPrefer.keys():
-			print(guyIndex[man],"proposes to"," ", end="")
+			#print(guyIndex[man],"proposes to"," ", end="")
 			for woman in guyPrefer[man]:
-				print(girlIndex[woman])
+				print(guyIndex[man],"proposes to"," ",girlIndex[woman])
 				if(woman not in girlEngaged):
 					engaged[man] = woman
 					girlEngaged[woman] = man
@@ -45,9 +53,12 @@ def matching(fm,engaged,girlEngaged):
 					newGuyIndex = girlPrefer[woman].index(man)
 					
 					if(currentGuyIndex > newGuyIndex):
+
 						engaged[man] = woman
 						girlEngaged[woman] = man
 						fm.remove(man)
+						del engaged[currentGuy]
+						fm.append(currentGuy)					
 						print(girlIndex[woman]," dumps", guyIndex[currentGuy])
 						print(guyIndex[man], " engaged to ", girlIndex[woman])
 						break
@@ -55,6 +66,7 @@ def matching(fm,engaged,girlEngaged):
 		break
 	return engaged
 def main():
+	t0 = time.process_time()
 	freeMen = []
 	engaged = {}
 	girlEngaged = {}
@@ -87,9 +99,21 @@ def main():
 			print(guyIndex[i], ",", end="")
 		print("")
 	engaged = matching(freeMen, engaged, girlEngaged)
+	
 	print("Pairing :")
 	for key, value in engaged.items():
-		print(guyIndex[key], value)
+		print(" ",guyIndex[key],"-", girlIndex[value])
+
 	#print(engaged)
-		
+	print("Elapsed wall clock time: ")
+	print("Elapsed CPU time: ", time.process_time() - t0)
+	print("Stable matchup") 
+	
+	#Taking User Input
+	person = input('Another trial? (y)es, (n)o ')
+	while(person == "y" or "yes"):
+		#person = input("Another trial? (yes), (n)o")
+		main()
+
+			
 main()
